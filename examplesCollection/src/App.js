4706c26addb8fb56f7c992b494b35231c21adb19
Abduino <abdrehmanredi0@gmail.com>
2022-10-01 12:07:00 -0300

    Example on React props with class

--- examplesCollection/src/App.js
+++ examplesCollection/src/App.js
@@ -3,11 +3,16 @@ import './App.css';
 import {Hello} from './components/Hello';
 /* import {WellCome} from './components/WellCome'; */
 import {WellCome} from './components/WellCome';
+import UserName from './components/PropExample';
+
+
+
 function App() {
   return (
     <div className="App">
       <Hello/>
       <WellCome/>
+      <UserName name="Abdurehman"/>
     </div>
   );
 }
---
+++ examplesCollection/src/components/PropExample.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Component } from "react";
+
+//props example with class component (statefull component)
+
+/* class UserName extends Component{
+    render(props){
+        return(
+            <div>
+                <h2>The user name is {this.props.name}</h2>
+            </div>
+        )
+    }
+}
+export default UserName */
+
