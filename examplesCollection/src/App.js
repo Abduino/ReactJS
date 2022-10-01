f612d653ec80be38a6177e7abf706a114cf37af5
Abduino <abdrehmanredi0@gmail.com>
2022-10-01 11:58:02 -0300

    Root folder import modified

--- examplesCollection/src/App.js
+++ examplesCollection/src/App.js
@@ -1,8 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
-import Hello from './components/Hello';
+import {Hello} from './components/Hello';
 /* import {WellCome} from './components/WellCome'; */
-import WellCome from './components/WellCome';
+import {WellCome} from './components/WellCome';
 function App() {
   return (
     <div className="App">
