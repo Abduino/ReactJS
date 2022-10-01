38bb11ecc20cc1b6dd1b2a2ede608666ab4fcefa
Abduino <abdrehmanredi0@gmail.com>
2022-10-01 14:09:06 -0300

    parent component added

---
+++ examplesCollection/src/components/ParentComponent.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react'
+import ChildComponent from './ChildComponent'
+
+class ParentComponent extends Component {
+    constructor(props) {
+        super(props)
+        this.state ={
+            parentName : 'Parent'
+        }
+        this.alerHandler = this.alerHandler.bind(this)
+    }
+    
+    alerHandler(){
+        alert(`hello parent `)
+    }
+    render(){
+        return (
+            <div>
+                <hr/>
+                <h2>parent page</h2>
+                <ChildComponent funName= {this.alerHandler}/>
+                
+                
+            </div>
+        )
+}
+}
+
+export default ParentComponent
