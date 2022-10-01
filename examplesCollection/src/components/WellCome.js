c7862424c17e983c35367f06f65dda94faaaefd1
Abduino <abdrehmanredi0@gmail.com>
2022-10-01 11:54:41 -0300

    Greeting component with stateful class

---
+++ examplesCollection/src/components/WellCome.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Component } from "react";
+
+class WellCome extends Component{
+    render(){
+        return (
+            <div>
+                <h2>Well Come Class</h2>
+            </div>
+        )
+
+    }
+
+}
+export default WellCome
