52cf7146fc81b1534607c7fbcd18a7f7c2581cc2
Abduino <abdrehmanredi0@gmail.com>
2022-10-01 11:52:45 -0300

    Hello world with Arrow function

--- examplesCollection/src/components/Hello.js
+++ examplesCollection/src/components/Hello.js
@@ -1,12 +1,21 @@
 import React from "react";
 
-function Hello(){
+/* function Hello(){
     return (
         <div>
             <h1>Hello World</h1>
         </div>
     )
+} */
+
+const Hello = () =>{
+    return(
+        <div>
+            <h2>Hello</h2>
+        </div>
+    )
 }
+
 
 export default Hello
 
